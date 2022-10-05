529dd78ce4fa512938bd273ed098446462496fb5
Nimet Çelik <kilicnimet@hotmail.com>
2022-10-05 15:49:22 +0300

    fonksiyonlar güncellendi

--- app.js
+++ app.js
@@ -5,28 +5,38 @@ let clearDOM = document.querySelector("#zero") // zero idli buton tanıtıldı
 let counter = 0 // counter sıfıra eşitlendi -- sayaç sıfırdan başlayacak
 counterDOM.innerHTML = counter // htmldeki h3 etiketine counter içindeki sayı yazdırıldı - şu an 0
 
+clearDOM.addEventListener("click", sifirla) // sıfırla butonuna basılınca sıfırla fonksiyonu çalıştırıldı - fonksiyon aşağıda
 increaseDOM.addEventListener("click", arttir) // artır butonuna basılınca artır fonksiyonu çalıştırıldı - fonksiyon aşağıda
 decreaseDOM.addEventListener("click", azalt) // azalt butonuna basılınca azalt fonksiyonu çalıştırıldı - fonksiyon aşağıda
-clearDOM.addEventListener("click", sifirla) // sıfırla butonuna basılınca sıfırla fonksiyonu çalıştırıldı - fonksiyon aşağıda
+
+function sifirla(){ // sifirla fonksiyonu yaratıldı
+    counter = 0 // counter değeri her ne olursa olsun sıfırlandı
+    counterDOM.innerHTML = counter // htmldeki h3 etiketine counterın son değeri yazıldı -- 0 yazar
+    if (counter === 0) { // eğer counter 0a eşitse 
+        clearDOM.classList.add("disabled") // htmldeki zero idli clearDOM elementine disabled classı eklendi -- böylece bootstrap class özellikleri ile buton pasif gözükecek
+    } else clearDOM.classList.remove("disabled") // htmldeki zero idli clearDOM elementinden disabled classı kaldırıldı -- böylece bootstrap class özellikleri ile buton aktif gözükecek
+        console.log(clearDOM.classList)
+}
 
 function arttir(){ // artır fonksiyonu yaratıldı
     counter += 1 // counter değeri 1 artırıldı 
     counterDOM.innerHTML = counter // htmldeki h3 etiketine counterın son değeri yazıldı
+    if (counter === 0) { // eğer counter 0a eşitse 
+        clearDOM.classList.add("disabled") // htmldeki zero idli clearDOM elementine disabled classı eklendi -- böylece bootstrap class özellikleri ile buton pasif gözükecek
+    } else clearDOM.classList.remove("disabled") // htmldeki zero idli clearDOM elementinden disabled classı kaldırıldı -- böylece bootstrap class özellikleri ile buton aktif gözükecek
+        console.log(clearDOM.classList)
 }
 
 function azalt(){ // azalt fonksiyonu yaratıldı
     counter -= 1 // counter değeri 1 azaltıldı 
     counterDOM.innerHTML = counter // htmldeki h3 etiketine counterın son değeri yazıldı
+    if (counter === 0) { // eğer counter 0a eşitse 
+        clearDOM.classList.add("disabled") // htmldeki zero idli clearDOM elementine disabled classı eklendi -- böylece bootstrap class özellikleri ile buton pasif gözükecek
+    } else clearDOM.classList.remove("disabled") // htmldeki zero idli clearDOM elementinden disabled classı kaldırıldı -- böylece bootstrap class özellikleri ile buton aktif gözükecek
+        console.log(clearDOM.classList)
 }
 
-function sifirla(){ // sifirla fonksiyonu yaratıldı
-    counter = 0 // counter değeri her ne olursa olsun sıfırlandı
-    counterDOM.innerHTML = counter // htmldeki h3 etiketine counterın son değeri yazıldı -- 0 yazar
-}
 
-    // burada sayı zaten sıfırsa sıfırla butonu boşuna çalışmasın diye bir if koşulu oluşturdum ama sorun var. Buton pasif oluyor ama aktifleşmiyor
-if (counter === 0) { // eğer counter 0a eşitse 
-    clearDOM.classList.add("disabled") // htmldeki zero idli clearDOM elementine disabled classı eklendi -- böylece bootstrap class özellikleri ile buton pasif gözükecek
-} else clearDOM.classList.remove("disabled") // htmldeki zero idli clearDOM elementinden disabled classı kaldırıldı -- böylece bootstrap class özellikleri ile buton aktif gözükecek
-    console.log(clearDOM.classList)
+
+
 
